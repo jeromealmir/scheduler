96b5b2cf704cf15c3e69f5acd9ac0f78a1295fa7
Jerome <101889450+jeromealmir@users.noreply.github.com>
2023-02-27 23:22:46 -0800

    implement setState values for student and interviewer

--- src/components/Appointment/Form.jsx
+++ src/components/Appointment/Form.jsx
@@ -3,6 +3,8 @@ import Button from "components/Button";
 import InterviewerList from "components/InterviewerList";
 
 export default function Form(props) {
+  const [student, setStudent] = useState(props.student || '');
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
