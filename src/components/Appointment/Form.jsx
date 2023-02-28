d2db12d0e6be96e4f4b21b4d3345ce5d8d894c28
Jerome <101889450+jeromealmir@users.noreply.github.com>
2023-02-27 23:28:03 -0800

    write event handlers for Cancel and Save buttons

--- src/components/Appointment/Form.jsx
+++ src/components/Appointment/Form.jsx
@@ -37,8 +37,8 @@ export default function Form(props) {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger >Cancel</Button>
-          <Button confirm >Save</Button>
+          <Button danger onClick={cancel}>Cancel</Button>
+          <Button confirm onClick={() => props.onSave(student, interviewer)}>Save</Button>
         </section>
       </section>
     </main>
