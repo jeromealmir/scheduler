3f6c57961fd40fd389599222188732197c246ac5
Jerome <101889450+jeromealmir@users.noreply.github.com>
2023-03-04 02:16:44 -0800

    refactor transition and back function to use ternary

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -7,15 +7,15 @@ export function useVisualMode(initial) {
   function transition(newMode, replace = false) {
     setMode(newMode)
   
+    //if replace is truthy, replace current mode in history otherwise add current mode to history
     replace ? history.splice(-1, 1, newMode) : setHistory(prev => [...prev, newMode])
-      
+
   }
 
   function back() {
 
-    if (history.length > 1) {
-      history.pop()
-    }
+    //remove last item from history only if history.length is more than one
+    history.length > 1 && history.pop()
     
     const prev = history.slice(-1).toString();
     setMode(prev)
