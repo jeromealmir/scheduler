da8ab763258c715699158442f7e1ab751d7b21b0
Jerome <101889450+jeromealmir@users.noreply.github.com>
2023-03-04 01:21:10 -0800

    write and implement back function and history array

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,10 +2,22 @@ import React, { useState } from "react";
 
 export function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
   function transition(newMode) {
     setMode(newMode)
+    setHistory(prev => [...prev, newMode])
   }
 
-  return { mode, transition };
+  function back() {
+
+    if (history.length > 1) {
+      history.pop()
+    }
+    
+    const prev = history.slice(-1).toString();
+    setMode(prev)
+  }
+
+  return { mode, transition, back };
 }
