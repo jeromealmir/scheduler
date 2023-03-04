89ed9a88b39a5f616fffd090fb93956e50dc5c53
Jerome <101889450+jeromealmir@users.noreply.github.com>
2023-03-03 19:52:20 -0800

    remove hardcoded appointments object

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -5,45 +5,6 @@ import DayList from "./DayList";
 import Appointment from "./Appointment";
 import { getAppointmentsForDay } from "helpers/selectors";
 
-// const appointments = {
-//   "1": {
-//     id: 1,
-//     time: "12pm",
-//   },
-//   "2": {
-//     id: 2,
-//     time: "1pm",
-//     interview: {
-//       student: "Lydia Miller-Jones",
-//       interviewer:{
-//         id: 3,
-//         name: "Sylvia Palmer",
-//         avatar: "https://i.imgur.com/LpaY82x.png",
-//       }
-//     }
-//   },
-//   "3": {
-//     id: 3,
-//     time: "2pm",
-//   },
-//   "4": {
-//     id: 4,
-//     time: "3pm",
-//     interview: {
-//       student: "Archie Andrews",
-//       interviewer:{
-//         id: 4,
-//         name: "Cohana Roy",
-//         avatar: "https://i.imgur.com/FK8V841.jpg",
-//       }
-//     }
-//   },
-//   "5": {
-//     id: 5,
-//     time: "4pm",
-//   }
-// };
-
 export default function Application(props) {
 
   const [state, setState] = useState({
@@ -51,7 +12,7 @@ export default function Application(props) {
     days: [],
     appointments: {}
   });
-  
+
   const dailyAppointments = getAppointmentsForDay(state, state.day);
   const setDay = day => setState(prev => ({...prev, day}));
 
