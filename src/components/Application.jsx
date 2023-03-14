ad007b2e2b43d2d829cf03dea26f636f3d1a6bbc
Jerome <101889450+jeromealmir@users.noreply.github.com>
2023-03-14 01:47:41 -0700

    define and implement cancelInterview function

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -31,6 +31,18 @@ export default function Application(props) {
     .catch(error => console.log(error))
   }
 
+  const cancelInterview = (id) => {
+    const appointment = {
+      ...state.appointments[id],
+      interview: null
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+    return axios.delete(`/api/appointments/${id}`).then(() => setState({...state, appointments}))
+  }
+
   const dailyAppointments = getAppointmentsForDay(state, state.day);
   
   const schedule = dailyAppointments.map(appointment => {
@@ -42,6 +54,7 @@ export default function Application(props) {
       time={appointment.time}
       interview={interview}
       bookInterview={bookInterview}
+      cancelInterview={cancelInterview}
       interviewers={dailyInterviewers}
       />
     );
