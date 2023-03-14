0fe6c537a88d7898a9f24a8996f3583c74ab31f5
Jerome <101889450+jeromealmir@users.noreply.github.com>
2023-03-14 01:51:40 -0700

    pass cancelInterview transition to DELETING mode

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -12,6 +12,7 @@ export default function Appointment(props) {
   const SHOW = "SHOW";
   const CREATE = "CREATE";
   const SAVING = "SAVING";
+  const DELETING = "DELETING";
 
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
@@ -39,11 +40,14 @@ export default function Appointment(props) {
       )}
       {mode === SHOW && (
         <Show
-          student={props.interview.student}
-          interviewer={props.interview.interviewer.name}
+          onDelete={() => {
+            transition(DELETING)
+            props.cancelInterview(props.id).then(() => transition(EMPTY))
+          }}
         />
       )}
       {mode === SAVING && <Status message="Saving"/>}
+      {mode === DELETING && <Status message="Deleting"/>}
     </article>
   );
 }
