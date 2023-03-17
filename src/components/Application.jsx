5c06b7587e1c611704bfc1bfe308423a7e87ce0b
Jerome <101889450+jeromealmir@users.noreply.github.com>
2023-03-17 01:06:29 -0700

    move setDay and useEffect to useApplicationData

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -30,19 +30,6 @@ export default function Application(props) {
     );
   });
 
-  const setDay = day => setState(prev => ({...prev, day}));
-
-  useEffect(() => {
-    Promise.all([
-      axios.get('api/days'),
-      axios.get('api/appointments'),
-      axios.get('api/interviewers')
-    ]).then((all) => {
-      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}))
-    })
- 
-  }, []);
-
   return (
     <main className="layout">
       <section className="sidebar">
