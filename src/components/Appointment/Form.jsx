51f3d3f91ac7926dc86c027312865bd6a2e3dc06
Jerome <101889450+jeromealmir@users.noreply.github.com>
2023-03-26 20:26:59 -0700

    change onClick to use validate function

--- src/components/Appointment/Form.jsx
+++ src/components/Appointment/Form.jsx
@@ -56,7 +56,7 @@ export default function Form(props) {
           <Button danger onClick={cancel}>
             Cancel
           </Button>
-          <Button confirm onClick={() => props.onSave(student, interviewer)}>
+          <Button confirm onClick={validate}>
             Save
           </Button>
         </section>
