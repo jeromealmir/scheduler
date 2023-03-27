0af55b790901e387e057822b4b9bd15400906000
Jerome <101889450+jeromealmir@users.noreply.github.com>
2023-03-26 20:26:35 -0700

    add error state

--- src/components/Appointment/Form.jsx
+++ src/components/Appointment/Form.jsx
@@ -5,6 +5,7 @@ import InterviewerList from "components/InterviewerList";
 export default function Form(props) {
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
 
   const reset = () => {
     setStudent("");
