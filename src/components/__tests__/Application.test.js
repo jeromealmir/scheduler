71b06d0425bf0417a2901cda3637fa1a9cd60212
Jerome <101889450+jeromealmir@users.noreply.github.com>
2023-03-27 15:33:37 -0700

    remove debug

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -29,7 +29,7 @@ describe("Application", () => {
   });
 
   it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
-    const { container, debug } = render(<Application />);
+    const { container } = render(<Application />);
 
     await waitForElement(() => getByText(container, "Tuesday"));
 
