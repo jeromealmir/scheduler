b1879f42e9b8c9224cc3c6043223288ccd939846
Jerome <101889450+jeromealmir@users.noreply.github.com>
2023-03-27 11:44:30 -0700

    write mock delete function

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -87,5 +87,14 @@ export default {
       })
     }
 
+  }),
+
+  delete: jest.fn((url) => {
+    if (url) {
+      return Promise.resolve({
+        status: 204,
+        statusText: "No Content"
+      })
+    }
   })
 };
