66debd1eb6f17e13fab855ff2ed91ee0ecbaa86d
Jerome <101889450+jeromealmir@users.noreply.github.com>
2023-03-27 11:44:11 -0700

    use url to pass dynamic data

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -80,7 +80,7 @@ export default {
   }),
   
   put: jest.fn((url) => {
-    if (url === `/api/appointments/1`) {
+    if (url) {
       return Promise.resolve({
         status: 204,
         statusText: "No Content"
