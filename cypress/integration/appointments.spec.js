0c4143cf0526fbed2ac3df9343d9bf5bfffb5d2d
Jerome <101889450+jeromealmir@users.noreply.github.com>
2023-03-30 05:26:00 -0700

    run commands before each test

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,2 +1,9 @@
 describe("should book an interview", () => {
+  beforeEach(() => {
+    cy.request("GET", "/api/debug/reset");
+
+    cy.visit("/");
+
+    cy.contains('[data-testid="day"]', "Monday");
+  });
 });
