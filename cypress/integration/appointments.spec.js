c18b437d08baaccf599d1f26095e55ac74f1247a
Jerome <101889450+jeromealmir@users.noreply.github.com>
2023-03-30 05:26:25 -0700

    write test that will book an interview

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -6,4 +6,18 @@ describe("should book an interview", () => {
 
     cy.contains('[data-testid="day"]', "Monday");
   });
+
+  it("should book an interview", () => {
+    cy.get("[alt=Add]")
+      .first()
+      .click();
+    
+    cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
+    cy.get("[alt='Sylvia Palmer']").click();
+
+    cy.contains("Save").click();
+
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Sylvia Palmer");
+  });
 });
