2b26244d096aaa02bd274592d29160459fc9417c
Jerome <101889450+jeromealmir@users.noreply.github.com>
2023-03-31 20:08:04 -0700

    add JSDocs comment

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,19 +1,24 @@
-//cypress end-to-end test for Appointment component
+/** 
+ * This test suite checks if a user can book, edit, and cancel an interview.
+ */
 
 describe("should book an interview", () => {
-  //reset database and visit root before each test run
+  /**
+   * Reset the server API, visit the application's homepage,
+   * and verify that Monday is displayed on the screen before each test
+   */
   beforeEach(() => {
     cy.request("GET", "/api/debug/reset");
-
     cy.visit("/");
-
     cy.contains('[data-testid="day"]', "Monday");
   });
 
+  /**
+   * This test case verifies that a user can book an interview by selecting a student name and interviewer,
+   * and then saving the appointment.
+   */
   it("should book an interview", () => {
     cy.get("[alt=Add]").first().click();
-
-    //find form input field by data-* attribute
     cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
     cy.get("[alt='Sylvia Palmer']").click();
 
@@ -23,10 +28,15 @@ describe("should book an interview", () => {
     cy.contains(".appointment__card--show", "Sylvia Palmer");
   });
 
+  /**
+   * Tests the functionality of editing an interview.
+   * It clicks on the "Edit" button of the first interview card, clears the student name input field,
+   * types in a new student name, selects a new interviewer, and clicks the "Save" button.
+   * Then it checks if the new student name and interviewer are displayed on the appointment card.
+   */
   it("should edit an interview", () => {
     cy.get("[alt='Edit']").first().click({ force: true });
 
-    //find form input field by data-* attribute
     cy.get("[data-testid=student-name-input]")
       .clear()
       .type("Lydia Miller-Jones");
@@ -39,6 +49,11 @@ describe("should book an interview", () => {
     cy.contains(".appointment__card--show", "Tori Malcolm");
   });
 
+  /**
+   * Tests the functionality of cancelling an interview.
+   * It clicks on the delete button of the first appointment card, confirms the deletion,
+   * and then checks that the card no longer exists.
+   */
   it("should cancel an interview", () => {
     cy.get("[alt='Delete']").first().click({ force: true });
 
--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,7 +1,13 @@
+/**
+ * Test suite for navigation functionality.
+ */
 describe("Navigation", () => {
+  // Visits the root URL of the application using Cypress.
   it("should visit root", () => {
     cy.visit("/");
   });
+
+  // Navigates to the Tuesday page and verifies that it is selected.
   it("should navigate to Tuesday", () => {
     cy.visit("/");
     cy.contains('[data-testid="day"]', "Tuesday")
