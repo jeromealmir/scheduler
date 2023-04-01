17a88acf002dcc569eb6f3a0c0b49b2ab6c6ecec
Jerome <101889450+jeromealmir@users.noreply.github.com>
2023-03-31 19:17:11 -0700

    move import object to bottom

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -1,8 +1,6 @@
 import React from "react";
-
-import { render, cleanup, fireEvent } from "@testing-library/react";
-
 import Form from "components/Appointment/Form";
+import { render, cleanup, fireEvent } from "@testing-library/react";
 
 afterEach(cleanup);
 
