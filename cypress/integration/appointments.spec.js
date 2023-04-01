155a9d1a6757d1f8155e3075fb5340acfbb4e594
Jerome <101889450+jeromealmir@users.noreply.github.com>
2023-03-31 18:39:19 -0700

    fix styling by prettier

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -8,10 +8,8 @@ describe("should book an interview", () => {
   });
 
   it("should book an interview", () => {
-    cy.get("[alt=Add]")
-      .first()
-      .click();
-    
+    cy.get("[alt=Add]").first().click();
+
     cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
     cy.get("[alt='Sylvia Palmer']").click();
 
@@ -22,14 +20,12 @@ describe("should book an interview", () => {
   });
 
   it("should edit an interview", () => {
-    cy.get("[alt='Edit']")
-      .first()
-      .click({ force: true });
-    
-    cy.get('[data-testid=student-name-input]')
+    cy.get("[alt='Edit']").first().click({ force: true });
+
+    cy.get("[data-testid=student-name-input]")
       .clear()
       .type("Lydia Miller-Jones");
-    
+
     cy.get("[alt='Tori Malcolm']").click();
 
     cy.contains("Save").click();
@@ -39,14 +35,12 @@ describe("should book an interview", () => {
   });
 
   it("should cancel an interview", () => {
-    cy.get("[alt='Delete']")
-      .first()
-      .click({ force: true });
-    
+    cy.get("[alt='Delete']").first().click({ force: true });
+
     cy.contains("Confirm").click();
 
-    cy.contains("Deleting").should("exist")
-    cy.contains("Deleting").should("not.exist")
+    cy.contains("Deleting").should("exist");
+    cy.contains("Deleting").should("not.exist");
     cy.contains(".appointment__card--show", "Archie Cohen").should("not.exist");
   });
 });
