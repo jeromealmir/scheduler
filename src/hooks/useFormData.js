f1ae0925c17f8a93a645815e4d3fb8e232967419
Jerome <101889450+jeromealmir@users.noreply.github.com>
2023-04-01 00:22:36 -0700

    export form state handling and helper functions

---
+++ src/hooks/useFormData.js
@@ -0,0 +1,59 @@
+/**
+ * A custom React hook that manages the state of a form.
+ * 
+ * @param {Object} props - The props object containing the initial state of the form.
+ * @param {string} props.student - The initial value of the student name input.
+ * @param {Object} props.interviewer - The initial value of the interviewer select input.
+ * @param {Function} props.onCancel - The function to call when the cancel button is clicked.
+ * @param {Function} props.onSave - The function to call when the save button is clicked.
+ * @returns {Object} - An object containing the current state of the form, as well as functions to update and validate the form.
+ */
+
+import { useState } from "react";
+
+export default function useFormData(props) {
+  const [state, setState] = useState({
+    student: props.student || "",
+    interviewer: props.interviewer || null,
+    error: "",
+  });
+
+  const setStudent = (value) =>
+    setState((prev) => ({ ...prev, student: value }));
+  const setInterviewer = (value) =>
+    setState((prev) => ({ ...prev, interviewer: value }));
+  const setError = (value) => setState((prev) => ({ ...prev, error: value }));
+
+  // Resets the state of the component by setting the student to an empty string and the interviewer to null.
+  const reset = () => {
+    setStudent("");
+    setInterviewer(null);
+  };
+
+  // Calls the reset function and the onCancel prop when the user cancels the appointment.
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  };
+
+  /**
+   * Validates the student name and interviewer selection before calling the onSave function.
+   * If the student name is blank or the interviewer is not selected, an error message is set.
+   */
+  const validate = () => {
+    if (state.student === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+
+    if (state.interviewer === null) {
+      setError("Please select an interviewer");
+      return;
+    }
+
+    setError("");
+    props.onSave(state.student, state.interviewer);
+  };
+
+  return { state, setStudent, setInterviewer, cancel, validate };
+}
