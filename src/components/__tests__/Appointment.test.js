78dd52f8396049e4ec510c4d023e1c2806977988
Jerome <101889450+jeromealmir@users.noreply.github.com>
2023-03-31 19:09:35 -0700

    remove cleanup as it is not needed in this test

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -1,9 +1,6 @@
 import React from "react";
 import Appointment from "components/Appointment";
-import { render, cleanup } from "@testing-library/react";
-
-
-afterEach(cleanup);
+import { render } from "@testing-library/react";
 
 describe("Appointment", () => {
   it("renders without crashing", () => {
