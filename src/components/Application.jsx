bbb480bc393d416a7400de0ade97d8fa3768d093
Jerome <101889450+jeromealmir@users.noreply.github.com>
2023-04-01 02:54:24 -0700

    export all functions to useApplicationData hook

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -2,39 +2,11 @@ import React from "react";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
-import {
-  getAppointmentsForDay,
-  getInterview,
-  getInterviewersForDay,
-} from "helpers/selectors";
 import useApplicationData from "hooks/useApplicationData";
 
 export default function Application(props) {
-  const { state, setDay, bookInterview, cancelInterview } =
+  const { state, setDay, appointments, getLastTime } =
     useApplicationData();
-
-  const interviewers = getInterviewersForDay(state, state.day);
-
-  const appointments = getAppointmentsForDay(state, state.day).map(
-    (appointment) => {
-      return (
-        <Appointment
-          key={appointment.id}
-          id={appointment.id}
-          time={appointment.time}
-          interview={getInterview(state, appointment.interview)}
-          bookInterview={bookInterview}
-          cancelInterview={cancelInterview}
-          interviewers={interviewers}
-        />
-      );
-    }
-  );
-
-  const getLastTime = appointments
-    .filter((appointment) => appointment.props && appointment.props.time)
-    .map((appointment) => `${parseInt(appointment.props.time.slice(0, -2)) + 1}pm`)
-    .pop();
 
   return (
     <main className="layout">
