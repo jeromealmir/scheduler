5126edf112ecc6212c272c771b05d0dde20be7a3
Jerome <101889450+jeromealmir@users.noreply.github.com>
2023-04-01 17:56:50 -0700

    break code to test circleci

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -27,7 +27,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={state.days} value={state.day} onChange={setDay} />
+          <DaayList days={state.days} value={state.day} onChange={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
